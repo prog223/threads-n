0ac191ed91209cba7a805e6750cbc7f1a3aa5dc5
prog223 <naneayvazyan4537@gmail.com>
2023-09-04 18:42:34 +0400

    empty commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -9,6 +9,7 @@ import { ClerkProvider } from '@clerk/nextjs';
 
 const inter = Inter({ subsets: ['latin'] });
 
+
 export const metadata: Metadata = {
 	title: 'Threads',
 	description: 'A Next.js 13 Meta Threads Application',
